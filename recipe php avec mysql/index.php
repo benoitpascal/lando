ffc845787a22712496badc8c049709cfda8d611e
DonBenito <pcl.ben.6@gmail.com>
2023-07-28 09:33:03 +0200

    Séparation des recettes php et php avec base de données

---
+++ recipe php avec mysql/index.php
@@ -0,0 +1,23 @@
+<?php
+    $titre = "<i>Lando</i>";
+
+    $head = '
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    </head>';
+?>
+
+<?php echo $head ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<body>
+    <h1>Bienvenu dans <?php echo $titre ?></h1>
+</body>
+</html>
+
